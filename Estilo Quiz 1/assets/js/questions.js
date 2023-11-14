198342bf267b6c6053d7a7da28d36d5d670590f0
Adrian-Pardal <139286701+Adrian-Pardal@users.noreply.github.com>
2023-11-14 17:18:07 -0300

    Add files via upload

---
+++ Estilo Quiz 1/assets/js/questions.js
@@ -0,0 +1,94 @@
+export default [
+    {
+        question: "Quanto é 2 x 2",
+        answers:[
+            {option: '22', correct: false},
+            {option: '2', correct: false},
+            {option: '3', correct: false},
+            {option: '4', correct: true},
+        ],
+    },
+    {
+        question: "Quanto é 1 x 2",
+        answers:[
+            {option: '2', correct: true},
+            {option: '11', correct: false},
+            {option: '2', correct: false},
+            {option: '1', correct: false},
+        ],
+    },
+    {
+        question: "Quanto é 1 + 1",
+        answers:[
+            {option: '2', correct: false},
+            {option: '0', correct: false},
+            {option: '11', correct: true},
+            {option: '1', correct: false},
+        ],
+    },
+    {
+        question: "Quanto é 2 + 1",
+        answers:[
+            {option: '2', correct: false},
+            {option: '21', correct: true},
+            {option: '1', correct: false},
+            {option: '3', correct: false},
+        ],
+    },
+    {
+        question: "Quanto é 42",
+        answers:[
+            {option: '44', correct: false},
+            {option: '2222', correct: true},
+            {option: '6', correct: false},
+            {option: '2', correct: false } ,
+        ],
+    },
+    {
+        question: "Quanto é 55",
+        answers:[
+            {option: '55555', correct: true},
+            {option: '10', correct: false},
+            {option: '55', correct: false},
+            {option: '25', correct: true } ,
+        ],
+    },
+    {
+        question: "Quem e mais Foda",
+        answers:[
+            {option: 'Batman', correct: false},
+            {option: 'Capitão America', correct: false},
+            {option: 'Homem de Ferro', correct: false},
+            {option: 'Robert Downey Jr', correct: true } ,
+        ],
+    },
+    {
+        question: "Gosta de Futbol",
+        answers:[
+            {option: 'Não', correct: false},
+            {option: 'SIM', correct: true},
+            {option: 'n', correct: false},
+            {option: 's', correct: false } ,
+        ],
+    },
+ 
+   {
+        question: "Quanto é 10 x 10",
+        answers:[
+            {option: '100', correct: false},
+            {option: '10101010101010101010', correct: true},
+            {option: '1010', correct: false},
+            {option: '0', correct: false } ,
+        ],
+    },
+
+    {
+        question: "Quanto é ",
+        answers:[
+            {option: '14', correct: false},
+            {option: '22', correct: false},
+            {option: '56', correct: false},
+            {option: 'tenta a Sorte', correct: true } ,
+        ],
+    },
+]
